108e641cc828a57e55fe71fc4b7561a519c0d529
player0-glitch <mokhaditlhalefo@gmail.com>
2026-01-14 12:26:50 +0000

    folder rename

---
+++ react_intro/task_0/dashboard/fileTransformer.js
@@ -0,0 +1,8 @@
+import path from 'path';
+export default {
+  process(sourceText, sourcePath) {
+    return {
+      code: `module.exports = ${JSON.stringify(path.basename(sourcePath))};`,
+    };
+  },
+};
